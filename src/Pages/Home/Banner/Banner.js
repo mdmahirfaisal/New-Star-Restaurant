9e65e91fe485fa1cf3a343696b55f889685ab3b3
unknown <rjmahir.faisal@gmail.com>
2021-11-30 12:48:09 +0600

    logo size updated

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,11 +8,11 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <div className=" homepage-bgimage py-5 mb-5">
+        <div className=" homepage-bgimage">
             <div className="mb-5 container">
-                <div className="mt-5">
-
-                    <h1 className="mb-3 text-start text-white"><Bounce bottom cascade duration={2000}>Best food waiting for your belly</Bounce></h1>
+                <div className="" style={{ paddingTop: '300px' }}>
+                    <h1 className="mb-3 text-start text-light"><Bounce bottom cascade duration={2000}>Best food waiting for your belly</Bounce></h1>
+                    <p className=" text-start text-light"><Bounce bottom cascade duration={2000}>Looking for a Franchise in Houston? Become Your Own Boss & Own a  Burn Boot Camp in Boston. <br /> Become one of our 400+ franchise partners and create a lasting change in your community.<br /> Worldwide Territories. Allowing For Fluid Change. Multi-Unit ownership. Low Cost Investment.</Bounce></p>
 
                     <Roll right duration={2000}>
                         <InputGroup className="mt-5 w-50">
